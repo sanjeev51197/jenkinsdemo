97edad0e2bef4c755197e4ec298f5af194e24126
Sanjeev Kumar <109436065+sanjeev51197@users.noreply.github.com>
2025-12-02 12:36:30 +0530

    Update Mycontroller.java

--- src/main/java/com/mvc/controller/Mycontroller.java
+++ src/main/java/com/mvc/controller/Mycontroller.java
@@ -1,5 +1,13 @@
 package com.mvc.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Mycontroller {
 
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Wow happy to see your learning!";
+    }
 }
